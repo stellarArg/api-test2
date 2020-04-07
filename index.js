d3d4bdd27000129a58d2b76c459f659ff1393080
Gabriel Verges <stellarusiv@gmail.com>
2020-04-07 11:10:17 -0300

    feat(secondCommit): use of middlewares, env and our first mocked database

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-// var << no se usa mas
-// let   << se define una vez pero se puede sobreescribir su valor
-// const << se define una vez y no se puede sobreescribir su valor
+// configurar un path 
+// require('dotenv').config({path: '../.env'}); 
+require('dotenv').config();
 
+// var << no se usa mas
+// let   << se define una vez pero se puede sobrescribir su valor
+// const << se define una vez y no se puede sobrescribir su valor
+
+const PORT = process.env.PORT;
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const express = require('express');
+
+let file;
+
+const loadFile = () => file = JSON.parse(fs.readFileSync(process.env.DB, 'utf-8'));
+
 const app = express();
 
 /**
-*	HTPP
+ * 
+ * Todos Los middlewares van antes de las rutas
+ * 
+ */
+app.use(bodyParser.json({limit: '20MB'}));
+
+/**
+ * ['1', 2, 3]
+ */
+app.use(bodyParser.urlencoded({}))
+
+
+/**
+*	HTTP
 *	GET  Se utiliza para traer datos
 *	POST Se utiliza para crear datos
 *	PUT  Se utiliza para modificar datos
@@ -19,27 +44,31 @@ const app = express();
 app.get('/', (req, res, next) => {
 	/**
 	*	req.
-	*		- Params  parametros de url  > localhost:2000/api/usuarios/:id,  req.params.id
-	*		- Query   parametros de la url pero query params, localhost:2000/api/usuarios?id=nuestra-id, req.query.id
-	*		- Body  Caso de que sea post y pust, req.body
+	*		- Params  parámetros de url  > localhost:2000/api/usuarios/:id,  req.params.id
+	*		- Query   parámetros de la url pero query params, localhost:2000/api/usuarios?id=nuestra-id, req.query.id
+	*		- Body  Caso de que sea post y put, req.body
 	*	res
 	*		res.sendStatus ( 200, 204, 403);
 	*		res.send ();
 	*
 	*	next
-	*		Continuar con nuestra ejecucion
+	*		Continuar con nuestra ejecución
 	*
 	*	app.get('/sincronizar', preSalvadoDatos, trabajoDeDatos)
 	*
 	*	preSalvadoDatos(req, res, next) => { .... se trabajan datos de pre guardado .... next(); }
 	*	trabajoDeDatos(req, res, next) => { .... trabajar datos ... res.send()}
-	*
 	*/
-	res.send('Hola Mundo');
+	loadFile();
+	res.send(file);
 });
 
-app.get('/query', (req, res, next) => { res.send(req.query.id)});
+app.get('/query', (req, res, next) => { res.send(req.query.id) });
 
-app.get('/:id', (req, res, next) => { res.send(req.params.id)});
+app.get('/:id', (req, res, next) => { 
+	loadFile();
+	const result = file.find(obj => parseInt(obj._id) === parseInt(req.params.id));
+	res.send(result)
+});
 
-app.listen(1000, () => console.log('Runing on port 1000'));
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
